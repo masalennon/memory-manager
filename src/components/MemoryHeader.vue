68caceafdebab2480565992522fce0f3a52bbec6
masalennon <competence.80@gmail.com>
2017-12-12 07:43:42 +0900

    succeeded to add a condition to show each card using v-if

---
+++ src/components/MemoryHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="title">
+      <h1>MemoryFeed</h1>
+      <hr/>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    margin-right: auto;
+    margin-left: auto;
+  }
+</style>
